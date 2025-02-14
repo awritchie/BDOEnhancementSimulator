a8b2f7ff1960705c83d964000f64c15f704075b8
andrew <drew.w.ritchie@gmail.com>
2025-02-14 09:35:15 -0600

    origins were buffed in ptr

--- BDOEnhancementSimulator/Program.cs
+++ BDOEnhancementSimulator/Program.cs
@@ -7,6 +7,10 @@ const double MaxInitialRate = 0.9;
 const double MinBaseRate = 0.01;
 var simulator = new Simulator(SimulationSize);
 
+//var results = await simulator.Start([new SimulationParameters("X Sov", 360, StackExtensions.SovereignEnhancements.Single(x => x.Level == 10), new AdditionalDetails(345, 15, 0))], true);
+//var report = results.BuildReport();
+//Console.WriteLine(report);
+
 IEnumerable<IEnumerable<EnhancementDetails>> sets = [StackExtensions.SovereignEnhancements, StackExtensions.KharazadEnhancements];
 foreach (var set in sets)
 {
@@ -19,10 +23,18 @@ foreach (var set in sets)
         var freeStack = 15u;
         var parameters = new List<SimulationParameters>();
 
+        var previousStackSize = 0u;
         for (uint stackSize = 0; stackSize < 100; stackSize += 10)
         {
             var additionalDetails = new AdditionalDetails(stackSize, freeStack, 0);
-            var finalStack = additionalDetails.GetFinalStack();
+            var finalStack = additionalDetails.GetFinalStack();            
+            if (finalStack == previousStackSize)
+            {
+                continue;
+            } else
+            {
+                previousStackSize = finalStack;
+            }
             var initialRate = level.SuccessRate(finalStack);
             if (initialRate >= MaxInitialRate || level.BasePercent < 100 * MinBaseRate)
             {
